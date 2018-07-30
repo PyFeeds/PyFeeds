43223d67412c209074816bcee86ca53482d4be52
Florian Preinstorfer <nblock@archlinux.us>
2018-07-30 14:28:05 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -28,4 +28,16 @@ setup(
         [console_scripts]
         feeds=feeds.cli:main
     """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Framework :: Scrapy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )

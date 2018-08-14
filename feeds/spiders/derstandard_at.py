dab3dc1d2b06ae628e9f26656ed3524177b1d524
Lukas Anzinger <lukas@lukasanzinger.at>
2018-08-14 19:33:01 +0200

    derStandard.at: Include caption of embedded content.

--- feeds/spiders/derstandard_at.py
+++ feeds/spiders/derstandard_at.py
@@ -90,7 +90,7 @@ class DerStandardAtSpider(FeedsXMLFeedSpider):
         replace_elems = {
             ".embedded-posting": "<p><em>Hinweis: Das eingebettete Posting ist nur "
             + "im Artikel verfügbar.</em></p>",
-            ".js-embed-container": "<p><em>Hinweis: Der eingebettete Inhalt ist nur "
+            ".js-embed-output": "<p><em>Hinweis: Der eingebettete Inhalt ist nur "
             + "im Artikel verfügbar.</em></p>",
         }
         il = FeedEntryItemLoader(

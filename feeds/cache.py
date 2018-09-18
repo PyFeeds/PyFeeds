17ade0ead0cc90d6724039820f284371dd6a2fbd
Lukas Anzinger <lukas@lukasanzinger.at>
2018-09-18 18:07:05 +0200

    Ignore if to be removed cache entry is not accessible.

--- feeds/cache.py
+++ feeds/cache.py
@@ -58,17 +58,19 @@ def cleanup_cache(cache_dir, expires):
 
 
 def remove_cache_entry(cache_entry_path, remove_parents=False):
-    if os.path.exists(cache_entry_path):
+    try:
         meta = read_meta(cache_entry_path)
-        if remove_parents:
-            logger.debug(
-                "Removing parent cache entries for URL {}".format(meta["response_url"])
-            )
-            spider_root = os.path.dirname(os.path.dirname(cache_entry_path))
-            for fingerprint in meta["parents"]:
-                path = os.path.join(spider_root, fingerprint[0:2], fingerprint)
-                remove_cache_entry(path, remove_parents=False)
-        logger.debug("Removing cache entry for URL {}".format(meta["response_url"]))
-        shutil.rmtree(cache_entry_path, ignore_errors=True)
-    else:
-        logger.debug("Cannot remove cache entry {}".format(cache_entry_path))
+    except FileNotFoundError:
+        return
+
+    if remove_parents:
+        logger.debug(
+            "Removing parent cache entries for URL {}".format(meta["response_url"])
+        )
+        spider_root = os.path.dirname(os.path.dirname(cache_entry_path))
+        for fingerprint in meta["parents"]:
+            path = os.path.join(spider_root, fingerprint[0:2], fingerprint)
+            remove_cache_entry(path, remove_parents=False)
+
+    logger.debug("Removing cache entry for URL {}".format(meta["response_url"]))
+    shutil.rmtree(cache_entry_path, ignore_errors=True)

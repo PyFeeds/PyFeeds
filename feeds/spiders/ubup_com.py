51ea1e54b151390e90977544aa5c7b898defefe2
Lukas Anzinger <lukas@lukasanzinger.at>
2018-09-19 21:46:54 +0200

    ubup.com: Add spider.

---
+++ feeds/spiders/ubup_com.py
@@ -0,0 +1,67 @@
+from urllib.parse import quote_plus as urlquote_plus, urljoin
+
+import scrapy
+
+from feeds.loaders import FeedEntryItemLoader
+from feeds.spiders import FeedsSpider
+
+
+class UbupComSpider(FeedsSpider):
+    name = "ubup.com"
+    allowed_domains = ["ubup.com"]
+    # Don't filter duplicates.
+    # This spider produces feeds with potentially overlapping items.
+    custom_settings = {"DUPEFILTER_CLASS": "scrapy.dupefilters.BaseDupeFilter"}
+
+    _title = "ubup"
+    _subtitle = "Deutschlands größter Second Hand-Onlineshop für Mode & Accessoires"
+    _base_url = "https://www.{}".format(name)
+    _icon = "https://www.{}/images/favicon.ico".format(name)
+    _scrape_pages = 3
+
+    def start_requests(self):
+        links = self.settings.get("FEEDS_SPIDER_UBUP_COM_LINKS")
+        if links:
+            self._links = links.split()
+        else:
+            self._links = ["katalog?sortiertnach=neueste"]
+
+        for link in self._links:
+            yield scrapy.Request(
+                urljoin(self._base_url, link),
+                meta={"dont_cache": True, "path": urlquote_plus(link)},
+            )
+
+    def feed_headers(self):
+        for link in self._links:
+            yield self.generate_feed_header(
+                link=urljoin(self._base_url, link),
+                path=urlquote_plus(link),
+            )
+
+    def parse(self, response):
+        for item in response.css(".thumbnail"):
+            il = FeedEntryItemLoader(selector=item, base_url=self._base_url)
+            il.add_css("title", ".item_brand_text ::text")
+            il.add_css("title", ".item-title ::text")
+            il.add_css("title", ".current-price ::text")
+            il.add_value(
+                "link",
+                response.urljoin(item.css(".item-link::attr(href)").extract_first()),
+            )
+            image_url = item.css(".item-image::attr(style)").re_first("'([^']+)'")
+            il.add_value("content_html", '<img src="{}">'.format(image_url))
+            il.add_css("content_html", ".item-des-container")
+            il.add_value("path", response.meta["path"])
+            yield il.load_item()
+
+        page = int(response.css(".pagination .active a::text").extract_first())
+        if page < self._scrape_pages:
+            next_page = response.css(
+                ".pagination .active + li a::attr(href)"
+            ).extract_first()
+            if next_page:
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    meta={"dont_cache": True, "path": response.meta["path"]},
+                )

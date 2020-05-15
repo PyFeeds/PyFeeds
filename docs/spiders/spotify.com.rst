d2bf961eda3a13dc5dd95eb2345435deff9db7d0
Lukas Anzinger <lukas@lukasanzinger.at>
2020-05-15 14:54:23 +0200

    Fix spotify.com spider.

--- docs/spiders/spotify.com.rst
+++ docs/spiders/spotify.com.rst
@@ -14,6 +14,10 @@ Add ``spotify.com`` to the list of spiders:
    spiders =
      spotify.com
 
+The market you are in (i. e. your country as an ISO 3166-1 alpha-2 country
+code) has to be specified in the config as well. For example, for Austria
+specify: ``market = AT``
+
 spotify.com supports different podcasts via the ``show`` parameter (one per
 line).
 
@@ -22,5 +26,6 @@ Example configuration:
 .. code-block:: ini
 
  [spotify.com]
+ market = AT
  shows =
      6u7pI0o0CUBQq0T1fwPgbj
--- feeds/spiders/spotify_com.py
+++ feeds/spiders/spotify_com.py
@@ -1,6 +1,4 @@
 import json
-import re
-from datetime import timedelta
 
 import scrapy
 
@@ -20,35 +18,26 @@ class SpotifyComSpider(FeedsSpider):
             self.logger.error("No shows given!")
             return
 
-        user_agent = self.settings.get("USER_AGENT")
+        self._market = self.settings.get("FEEDS_SPIDER_SPOTIFY_COM_MARKET")
+        if not self._market:
+            self.logger.error("Please specify a market (country code) in the config!")
+            return
 
         yield scrapy.Request(
-            "https://open.spotify.com",
-            meta={
-                "cache_expires": timedelta(hours=1),
-                "handle_httpstatus_list": [302],
-                "dont_redirect": True,
-            },
-            # Spotify checks the user agent and complains if it doesn't find a supported
-            # browser.
-            headers={"User-Agent": "{} Firefox/67.0".format(user_agent)},
+            "https://open.spotify.com/get_access_token?"
+            + "reason=transport&productType=web_player",
+            meta={"dont_cache": True},  # Only valid for a very short time.
         )
 
     def parse(self, response):
-        access_token = None
-        cookies = response.headers.getlist("Set-Cookie")
-        for cookie in cookies:
-            cookie = cookie.decode("ascii")
-            if cookie.startswith("wp_access_token="):
-                access_token = re.findall("wp_access_token=([^;]*)", cookie)[0]
-                break
-
+        response = json.loads(response.text)
+        access_token = response["accessToken"]
         for show in self._shows:
             yield scrapy.Request(
-                "https://api.spotify.com/v1/shows/{}".format(show),
+                f"https://api.spotify.com/v1/shows/{show}?market={self._market}",
                 self._parse_show,
-                meta={"show": show, "dont_cache": True},
-                headers={"Authorization": "Bearer {}".format(access_token)},
+                meta={"spotify_show": show, "dont_cache": True},
+                headers={"Authorization": f"Bearer {access_token}"},
             )
 
     def feed_headers(self):
@@ -62,7 +51,7 @@ class SpotifyComSpider(FeedsSpider):
             link=result["external_urls"]["spotify"],
             icon=result["images"][-1]["url"],
             logo=result["images"][0]["url"],
-            path=response.meta["show"],
+            path=response.meta["spotify_show"],
         )
 
         for episode in result["episodes"]["items"]:
@@ -71,5 +60,5 @@ class SpotifyComSpider(FeedsSpider):
             il.add_value("updated", episode["release_date"])
             il.add_value("title", episode["name"])
             il.add_value("content_html", episode["description"])
-            il.add_value("path", response.meta["show"])
+            il.add_value("path", response.meta["spotify_show"])
             yield il.load_item()

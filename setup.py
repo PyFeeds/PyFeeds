1f3961741b74f55bf3ff90f9d4c95e362c0be6da
Florian Preinstorfer <nblock@archlinux.us>
2020-05-15 11:38:40 +0200

    Drop support for Python 3.5
    This allows the use of f-strings.
    
    * Ubuntu 18.04 has 3.6
    * Debian Buster has 3.7

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
